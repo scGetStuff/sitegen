5728ec782e3459fc74c6c0c408d735c2c3feb92b
Scott <67354101+scGetStuff@users.noreply.github.com>
2025-02-08 14:53:03 -0500

    add htmlnode

---
+++ src/htmlnode.py
@@ -0,0 +1,35 @@
+# An HTMLNode without a tag will just render as raw text
+# An HTMLNode without a value will be assumed to have children
+# An HTMLNode without children will be assumed to have a value
+# An HTMLNode without props simply won’t have any attributes
+
+from typing import Self
+
+
+class HTMLNode:
+    def __init__(
+        self,
+        tag: str = None,
+        value: str = None,
+        children: list[Self] = None,
+        props: dict[str, str] = None,
+    ):
+        self.tag = tag
+        self.value = value
+        self.children = children
+        self.props = props
+
+    def to_html(self):
+        raise NotImplementedError()
+
+    def props_to_html(self):
+        if self.props == None:
+            return " None"
+        return "".join(
+            list(map(lambda item: f' {item[0]}="{item[1]}"', self.props.items()))
+        )
+
+    def __repr__(self):
+        s = f"HTMLNode: <{self.tag}{self.props_to_html()}>\nchildren:{ \
+            len(self.children) if self.children else 0}\nvalue:{self.value}"
+        return s
---
+++ src/test_htmlnode.py
@@ -0,0 +1,43 @@
+import unittest
+from htmlnode import HTMLNode
+
+css1 = {
+    "class": "stuff",
+}
+children = ["h1", "ul"]
+value = "This is an HTML node"
+
+
+class TestHTMLNode(unittest.TestCase):
+
+    # need to check for any error caused by any properties being None
+    # all properties are accesed by __repr__()
+    def test_None(self):
+        node = HTMLNode(None, None, None, None)
+        isBroken = False
+        try:
+            s = str(node)
+        except Exception:
+            isBroken = True
+        self.assertFalse(isBroken, "something being 'None' broke stuff")
+
+    def test_props_to_html(self):
+        node = HTMLNode("div", value, children, css1)
+        expected = ' class="stuff"'
+        self.assertEqual(node.props_to_html(), expected)
+
+    def test_to_html(self):
+        node = HTMLNode("div", value, children, css1)
+        self.assertRaises(NotImplementedError, node.to_html)
+
+    def test_repr(self):
+        node = HTMLNode("div", value, children, css1)
+        expected = f'HTMLNode: <div class="stuff">'
+        s = str(node)
+        self.assertEqual(s.find(expected), 0)
+        self.assertEqual(s.find("children:2") > -1, True)
+        self.assertEqual(s.find(value) > -1, True)
+
+
+if __name__ == "__main__":
+    unittest.main()
