dad2d1f5322ce063bdc5439fcaee0edcd840dc75
Scott <67354101+scGetStuff@users.noreply.github.com>
2025-02-24 14:17:47 -0500

    copy static to public

--- src/main.py
+++ src/main.py
@@ -1,5 +1,36 @@
+import os
+import shutil
+
+
 def main():
     print("Build a Static Site Generator")
+    copyAllFiles("static", "public")
+
+
+def copyAllFiles(fromDir: str, toDir: str):
+
+    if os.path.exists(toDir):
+        shutil.rmtree(toDir)
+
+    copyThing(fromDir, toDir)
+
+
+def copyThing(fromThing: str, toThing: str):
+
+    if os.path.isfile(fromThing):
+        shutil.copy(fromThing, toThing)
+        # print(f"FILE: {fromThing}")
+        return
+
+    os.mkdir(toThing)
+    content = os.listdir(fromThing)
+    # print(f"DIR: {fromThing}\n{content}")
+
+    for thing in content:
+        src = os.path.join(fromThing, thing)
+        dest = os.path.join(toThing, thing)
+        # print(f"\nFROM: {src}\nTO: {dest}")
+        copyThing(src, dest)
 
 
 if __name__ == "__main__":
